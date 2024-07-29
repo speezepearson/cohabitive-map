88cb5907e0c412013777646caa947f7a7591f340
Spencer Pearson <speeze.pearson@gmail.com>
2024-07-29 00:00:11 -0700

    define 6 HexTypes, and flip between them when disks are clicked

--- SphereThing.gd
+++ SphereThing.gd
@@ -1,28 +1,33 @@
 extends Node3D
 
-var Number_Generator = RandomNumberGenerator.new()
+var hex_type: HexTypeLib.HexType
 var material: Material
+
+const hex_type_colors = {
+	HexTypeLib.HexType.MOUNTAIN: Color(0.5, 0.5, 0.5, 1.0),
+	HexTypeLib.HexType.VOLCANO:  Color(0.8, 0.5, 0.5, 1.0),
+	HexTypeLib.HexType.WATER:    Color(0.0, 0.3, 1.0, 1.0),
+	HexTypeLib.HexType.ICE:      Color(0.5, 0.8, 1.0, 1.0),
+	HexTypeLib.HexType.GRASS:    Color(0.5, 1.0, 0.5, 1.0),
+	HexTypeLib.HexType.FOREST:   Color(0.0, 0.5, 0.0, 1.0),
+}
+
+func set_hex_type(t: HexTypeLib.HexType):
+	self.hex_type = t
+	self.material.albedo_color = hex_type_colors[self.hex_type]
 
 func _ready():
 	# duplicate override material so we can edit it without affecting all instances
 	self.material = $SphereMesh3D.get_surface_override_material(0).duplicate()
 	$SphereMesh3D.set_surface_override_material(0, self.material)
 
+	set_hex_type(HexTypeLib.random_type())
+
 func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			self.randomize_color()
+			set_hex_type(HexTypeLib.inverses[self.hex_type])
 
-
-func randomize_color():
-	self.Number_Generator.randomize()
-	var new_red = self.Number_Generator.randf_range(0.0, 1.0)
-	self.Number_Generator.randomize()
-	var new_green = self.Number_Generator.randf_range(0.0, 1.0)
-	self.Number_Generator.randomize()
-	var new_blue = self.Number_Generator.randf_range(0.0, 1.0)
-	var new_color = Vector3(new_red, new_green, new_blue).normalized()
-	self.material.albedo_color = Color(new_color.x, new_color.y, new_color.z, 1.0)
 
 
 func _on_Area_3d_input_event(camera, event, position, normal, shape_idx):
