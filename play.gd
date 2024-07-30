646c75e980f70454b5899782695d99fa351298a0
Spencer Pearson <speeze.pearson@gmail.com>
2024-07-29 21:31:08 -0700

    pull player into separate scene

--- play.gd
+++ play.gd
@@ -61,3 +61,7 @@ func _input(event: InputEvent):
 
 func _process(delta):
 	move_camera(delta)
+
+
+func _on_player_clicked():
+	print('click!')
---
+++ player.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+signal Clicked
+func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			Clicked.emit()
